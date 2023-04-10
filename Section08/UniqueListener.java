3a30398d3c9436b7e02093fe4fbe9c07dfbab351
TranTienVan <87886466+TranTienVan@users.noreply.github.com>
2023-04-10 22:50:48 +0700

    fox

--- Section08/UniqueListener.java
+++ Section08/UniqueListener.java
@@ -13,17 +13,29 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class UniqueListener {
 
     public static class MusicDataMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         private Text trackId = new Text();
+        private final static IntWritable one = new IntWritable(1);
+        private ArrayList<String> user_ids;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            ArrayList<String> user_ids = new ArrayList<String>();
+        }
 
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String[] fields = value.toString().split("\\|");
-            trackId.set(fields[1]);
-            context.write(new Text("unique_listeners"), new IntWritable(Integer.parseInt(fields[0])));
+
+            user_ids.add(fields[0]);
+
+            // context.write(new Text("unique_listeners"), new
+            // IntWritable(Integer.parseInt(fields[0])));
+
             context.write(new Text("track_shares"), new IntWritable(Integer.parseInt(fields[2])));
             context.write(new Text("radio_listens"), new IntWritable(Integer.parseInt(fields[3])));
             context.write(new Text("radio_skips"), new IntWritable(Integer.parseInt(fields[4])));
@@ -32,6 +44,16 @@ public class UniqueListener {
                 context.write(trackId, new IntWritable(1));
             }
         }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            // Output the graph
+            HashSet<String> myHashSet = new HashSet<String>(user_ids);
+            int count = myHashSet.size();
+
+            context.write(new Text("Number of unique listeners"), new IntWritable(count));
+        }
+
     }
 
     public static class MusicDataReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
