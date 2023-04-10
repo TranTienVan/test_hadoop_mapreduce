52a6c26237bf3195118a89af4233f53d39acc0f4
TranTienVan <87886466+TranTienVan@users.noreply.github.com>
2023-04-10 22:04:34 +0700

    fix

--- Section07/DeIdentifyData.java
+++ Section07/DeIdentifyData.java
@@ -11,44 +11,27 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
+import Section07.EncryptModule;
 
 public class DeIdentifyData {
-    private static final String ALGORITHM = "AES";
-    private static final String KEY = "Hadoop Lab 2"; // Replace with your own secret key
-
-    public static String encrypt(String input) {
-        try {
-            SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            byte[] encryptedBytes = cipher.doFinal(input.getBytes());
-            return Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (Exception e) {
-            return null;
-        }
-
-    }
 
     public static class PatientDataMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             // Split the input record by comma
             String[] fields = value.toString().split(",");
-
+            EncryptModule encrypt_moduler = new EncryptModule();
             // Construct the output value as comma-separated fields
             String outputValue = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s",
-                    encrypt(fields[0]),
-                    encrypt(fields[1]),
-                    encrypt(fields[2]),
-                    encrypt(fields[3]),
-                    encrypt(fields[4]),
-                    encrypt(fields[5]),
-                    encrypt(fields[6]),
-                    encrypt(fields[7]),
-                    encrypt(fields[8]));
+                    encrypt_moduler.encrypt(fields[0]),
+                    encrypt_moduler.encrypt(fields[1]),
+                    encrypt_moduler.encrypt(fields[2]),
+                    encrypt_moduler.encrypt(fields[3]),
+                    encrypt_moduler.encrypt(fields[4]),
+                    encrypt_moduler.encrypt(fields[5]),
+                    encrypt_moduler.encrypt(fields[6]),
+                    encrypt_moduler.encrypt(fields[7]),
+                    encrypt_moduler.encrypt(fields[8]));
 
             // Write the output value to the reducer
             context.write(NullWritable.get(), new Text(outputValue));
---
+++ Section07/EncryptModule.java
@@ -0,0 +1,32 @@
+package Section07;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptModule {
+    private static final String ALGORITHM = "AES";
+    private static final String KEY = "MySecretKey12345"; // Replace with your own secret key
+
+    public static String encrypt(String input) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            byte[] encryptedBytes = cipher.doFinal(input.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            System.err.println("Error encrypting string: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String originalString = "This is a secret message";
+        String encryptedString = encrypt(originalString);
+
+        System.out.println("Original string: " + originalString);
+        System.out.println("Encrypted string: " + encryptedString);
+    }
+
+}
